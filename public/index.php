643df6b2fb6c67b280d9c15749d6849da9af81d4
xoaltie <mezencevadaria96@yandex.ru>
2025-05-19 12:19:46 +0700

    init

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello world");
+    return $response;
+});
+
+$app->run();
